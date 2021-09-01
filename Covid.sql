caf5f9e32ae3917ebd81e45203c621cedb4a23b3
DimaYakovlevAndr <80648603+DimaYakovlevAndr@users.noreply.github.com>
2021-09-01 22:02:06 +0300

    Update Covid.sql

--- Covid.sql
+++ Covid.sql
@@ -9,40 +9,40 @@ Select Location, date, total_cases,total_deaths, (total_deaths/total_cases)*100 
 from PortfolioCovid.dbo.Deaths
 order by 1,2
 
--- Ïîñ÷èòàòü çàðàçíîñòü 
+-- Посчитать заразность 
 Select Location, date, Population, total_cases,  (total_cases/population)*100 as contagiousness
 From PortfolioCovid.dbo.Deaths
 order by 1,2
 
 
--- Ïîñ÷èòàòü ñòðàíû ñ íàèáîëüøèì êîë-âîì çàðàæåíèé 
+-- Посчитать страны с наибольшим кол-вом заражений 
 select location, population, MAX (total_cases) as HighestInfectionCount, Max((total_cases/population))*100 as contagiousness
 from PortfolioCovid.dbo.Deaths 
 Group by Location, Population
 order by contagiousness desc
 
--- Ïîñ÷èòàòü ñòðàíû ñ ñàìîé âûñîêîé ñìåòðíîñòüþ
+-- Посчитать страны с самой высокой сметрностью
 select location, MAX(cast(total_deaths as int)) as TotalDeaths
 from PortfolioCovid.dbo.Deaths 
 Where continent is not null 
 Group by Location
 order by TotalDeaths desc
 
--- Ïîñ÷èòàòü êîíòèíåíòû ñ ñàìîé âûñîêîé ñìåòðíîñòüþ
+-- Посчитать континенты с самой высокой сметрностью
 select location, MAX(cast(total_deaths as int)) as TotalDeaths
 from PortfolioCovid.dbo.Deaths 
 Where continent is null 
 Group by location
 order by TotalDeaths desc
 
--- Îáùèå öèôðû
+-- Общие цифры
 Select SUM(new_cases) as total_cases, SUM(cast(new_deaths as int)) as total_deaths, SUM(cast(new_deaths as int))/SUM(New_Cases)*100 as DeathPercentage
 From PortfolioCovid.dbo.Deaths 
 where continent is not null 
 order by 1,2
 
--- Ïðîöåíò íàñåëåíèÿ, ïîëó÷èâøèé õîòÿ áû îäíó äîçó âàêöèíó 
--- Ñîçäàòü êîëîíêó RollingPeopleVaccinated, â êîòîðîé ñóììèðóåòñÿ êîë-âî âàêöèíèðîâàííûõ
+-- Процент населения, получивший хотя бы одну дозу вакцину 
+-- Создать колонку RollingPeopleVaccinated, в которой суммируется кол-во вакцинированных
 select death.continent, death.location,  death.date, death.population, vacs.new_vaccinations
 , SUM(CONVERT(int,vacs.new_vaccinations)) OVER (Partition by death.Location order by death.Date) as RollingPeopleVaccinated
 from PortfolioCovid.dbo.Deaths death
@@ -52,7 +52,7 @@ Join PortfolioCovid.dbo.Vaccinations vacs
 where death.continent is not null
 order by 1,2
 
---Ïîñ÷èòàòü ïðîöåíò
+--Посчитать процент
 With VaccinatedPercentage (Continent, Location, Date, Population, New_Vaccinations, RollingPeopleVaccinated)
 as
 (
@@ -67,7 +67,7 @@ where death.continent is not null
 Select *, (RollingPeopleVaccinated/Population)*100
 From VaccinatedPercentage
 
--- Ñäåëàòü òàáëèöó ïî âàêöèíèðîâàííûì
+-- Сделать таблицу по вакцинированным
 
 DROP Table if exists #PercentPopulationVaccinated
 Create Table #PercentPopulationVaccinated
@@ -92,7 +92,7 @@ Select *, (RollingPeopleVaccinated/Population)*100
 From #PercentPopulationVaccinated
 
 
--- Ñîçäàåì view äëÿ ïîñëåäóþùåé âèçóàëèçàöèè
+-- Создаем view для последующей визуализации
 DROP VIEW if exists PercentPopulationVaccinated ;
 GO
 
